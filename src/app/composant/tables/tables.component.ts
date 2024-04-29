7684b94b204a796026fc87df39c9613d7ca2c373
Yves Donald Fotso <70369672+donaldk12345@users.noreply.github.com>
2024-04-29 16:22:10 +0200

    Update tables.component.ts

--- src/app/composant/tables/tables.component.ts
+++ src/app/composant/tables/tables.component.ts
@@ -7,54 +7,25 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit, OnChanges{
-  @Input() data:any;
-  @Input() monSort:any;
-  @Input() colum:any[]=[];
-  @Input() loading:any;
-  selectedProducts: any[] = [];
-  @Output() datas: EventEmitter<any> = new EventEmitter<any>()
-  static valuetab=0;
-  // @Output() datas2: EventEmitter<any> = new EventEmitter<any>()
+  
 
-  // loading :boolean = true
+
   constructor() {
-    console.log("sssssssss",this.data);
-
-    this.selectedProducts = this.data
-    if(this.selectedProducts && this.selectedProducts.length >0){
-
-      TablesComponent.valuetab= this.selectedProducts.length
-    }
+   
 
   }
 
   ngOnInit(): void {
-/*     console.log(this.data);
 
-    console.log("Mon tableau date", this.data);
-    console.log("Mon tableau couml", this.colum); */
-
-    console.log("Mon dis data ", this.data);
 
   }
 
   ngOnChanges() :void {
-    console.log("Mon tableau date chancge", this.data);
-    console.log("Mon tableau couml chancge", this.colum);
-    if(this.data){}
-    if(this.data && this.data.length!=TablesComponent.valuetab){
-
-      this.loading = false
-      this.selectedProducts=[];
-    }
+ 
 
   }
 
-  onRowSelect(dat: any) : void {
-    let tr= []
-    tr = this.selectedProducts.filter(r => r!= null && r.id != null && r.id > 0 );
-    this.datas.emit(tr)
-  }
+
 
 
 }
